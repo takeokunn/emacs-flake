e51161b4cb69f6af1e1b9fbf07391daf98349ff9
takeokunn <bararararatty@gmail.com>
2024-08-23 09:07:55 +0900

    Update

--- emacs.el
+++
@@ -1,5 +0,0 @@
-(setq user-full-name "hoge")
-
-(require 'php-mode)
-
-(use-package magit :ensure t)
--- flake.nix
+++ flake.nix
@@ -8,13 +8,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-stable.follows = "nixpkgs";
     };
-    nix-mode = {
-      url = "github:NixOS/nix-mode";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, emacs-overlay, nix-mode }:
+  outputs = { self, nixpkgs, emacs-overlay }:
     let
       systems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -35,8 +31,7 @@
             # NB: Config files cannot contain unicode characters, since
             #     they're being parsed in nix, which lacks unicode
             #     support.
-            config = ./emacs.org;
-            # config = ./emacs.el;
+            config = ./index.org;
 
             # Whether to include your config as a default init file.
             # If being bool, the value of config is used.
@@ -52,15 +47,6 @@
             # Package is optional, defaults to pkgs.emacs
             package = pkgs.emacs;
 
-            # By default emacsWithPackagesFromUsePackage will only pull in
-            # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
-            # Setting `alwaysEnsure` to `true` emulates `use-package-always-ensure`
-            # and pulls in all use-package references not explicitly disabled via
-            # `:ensure nil` or `:disabled`.
-            # Note that this is NOT recommended unless you've actually set
-            # `use-package-always-ensure` to `t` in your config.
-            alwaysEnsure = true;
-
             # For Org mode babel files, by default only code blocks with
             # `:tangle yes` are considered. Setting `alwaysTangle` to `true`
             # will include all code blocks missing the `:tangle` argument,
@@ -71,7 +57,22 @@
             alwaysTangle = true;
 
             # Optionally provide extra packages not in the configuration file.
-            extraEmacsPackages = epkgs: [ epkgs.php-mode ];
+            extraEmacsPackages = epkgs: with epkgs; [
+              apache-mode
+              bazel
+              bison-mode
+              cask-mode
+              cfn-mode
+              clojure-mode
+              cmake-mode
+              coffee-mode
+              cmake-mode
+              crontab-mode
+              csharp-mode
+              csv-mode
+              cuda-mode
+              php-mode
+            ];
           };
         });
     };
